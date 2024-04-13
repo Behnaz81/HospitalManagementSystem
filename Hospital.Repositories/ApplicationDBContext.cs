3832e345cc88cb116d1a9c7c5332636a4a0aa7ac
Behnaz MohammdHasani <18behnaz@gmail.com>
2024-04-13 23:00:17 +0330

    Add project files.

---
+++ Hospital.Repositories/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Hospital.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Repositories
+{
+    public class ApplicationDBContext : IdentityDbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
